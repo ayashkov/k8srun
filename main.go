342a33a053f97d6c19c33e26a2eab79f4adbfcf7
Alexei Yashkov <alexei.yashkov@yashkov.org>
2023-03-04 16:55:13 -0500

    Access image and command parameters from command line

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+type workload struct {
+	image   string
+	command string
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	workload, err := configure()
+
+	if err != nil {
+		fmt.Println(err)
+
+		os.Exit(1)
+	}
+
+	fmt.Println(workload.image)
+	fmt.Println(workload.command)
+}
+
+func configure() (*workload, error) {
+	argc := len(os.Args)
+
+	if argc < 2 {
+		return nil, fmt.Errorf("usage: %s image [command]", os.Args[0])
+	}
+
+	workload := workload{
+		image:   os.Args[1],
+		command: "",
+	}
+
+	if argc > 2 {
+		workload.command = os.Args[2]
+	}
+
+	return &workload, nil
 }
