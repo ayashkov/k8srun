455046eeeb676756dcb8fe76211ab1fde582c4e6
Alexei Yashkov <alexei.yashkov@yashkov.org>
2023-03-05 12:51:40 -0500

    Use Cobra for argument parsing

--- main.go
+++ main.go
@@ -4,48 +4,47 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/spf13/cobra"
 )
 
-type workload struct {
-	namespace string
-	image     string
-	command   string
-}
-
 func main() {
-	workload, err := configure()
+	var runCommand = &cobra.Command{
+		Use:   "go-study [flags] image [-- command [args...]]",
+		Short: "AutoSys to Kubernetes bridge",
+		Long: `This is an attempt to implement a bridge between AutoSys
+scheduler and a Kubernetes cluster. The goal is to be able to execute
+Kubernetes workload from AutoSys jobs.`,
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			namespace := cmd.Flags().Lookup("namespace").Value.String()
+			job := cmd.Flags().Lookup("job").Value.String()
+			command := ""
 
-	if err != nil {
+			if len(args) > 1 {
+				command = args[1]
+			}
+
+			fmt.Println("Namespace", namespace)
+			fmt.Println("Job", job)
+			fmt.Println("Image", args[0])
+			fmt.Println("Command", command)
+		},
+	}
+
+	runCommand.PersistentFlags().StringP("namespace", "n", "",
+		"The namespace for creating the pod")
+	runCommand.Flags().StringP("job", "j",
+		normalize(os.Getenv("AUTO_JOB_NAME")),
+		"The job name to use for naming the pod")
+
+	if err := runCommand.Execute(); err != nil {
 		fmt.Println(err)
-
 		os.Exit(1)
 	}
-
-	fmt.Println(workload.namespace)
-	fmt.Println(workload.image)
-	fmt.Println(workload.command)
-}
-
-func configure() (*workload, error) {
-	argc := len(os.Args)
-
-	if argc < 2 {
-		return nil, fmt.Errorf("usage: %v image [command]", os.Args[0])
-	}
-
-	workload := workload{
-		namespace: normalize(os.Getenv("AUTO_JOB_NAME")),
-		image:     os.Args[1],
-		command:   "",
-	}
-
-	if argc > 2 {
-		workload.command = os.Args[2]
-	}
-
-	return &workload, nil
 }
 
 func normalize(s string) string {
-	return strings.ReplaceAll(strings.TrimSpace(strings.ToLower(s)), "_", "-")
+	return strings.ReplaceAll(strings.TrimSpace(strings.ToLower(s)),
+		"_", "-")
 }
