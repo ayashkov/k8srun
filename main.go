c0d3a83888efc7b06c40fcad91cbbef69fc2b004
Alexei Yashkov <alexei.yashkov@yashkov.org>
2023-03-05 13:16:15 -0500

    Refactor the run command

--- main.go
+++ main.go
@@ -9,16 +9,23 @@ import (
 )
 
 func main() {
-	var runCommand = &cobra.Command{
+	if err := runCommand().Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func runCommand() *cobra.Command {
+	var namespace string
+	var job string
+	var cmd = &cobra.Command{
 		Use:   "go-study [flags] image [-- command [args...]]",
 		Short: "AutoSys to Kubernetes bridge",
 		Long: `This is an attempt to implement a bridge between AutoSys
-scheduler and a Kubernetes cluster. The goal is to be able to execute
-Kubernetes workload from AutoSys jobs.`,
+scheduler and a Kubernetes cluster. The goal is to be able
+to execute Kubernetes workload from AutoSys jobs.`,
 		Args: cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			namespace := cmd.Flags().Lookup("namespace").Value.String()
-			job := cmd.Flags().Lookup("job").Value.String()
 			command := args[1:]
 
 			fmt.Println("Namespace", namespace)
@@ -28,16 +35,13 @@ Kubernetes workload from AutoSys jobs.`,
 		},
 	}
 
-	runCommand.PersistentFlags().StringP("namespace", "n", "",
+	cmd.PersistentFlags().StringVarP(&namespace, "namespace", "n", "",
 		"The namespace for creating the pod")
-	runCommand.Flags().StringP("job", "j",
+	cmd.Flags().StringVarP(&job, "job", "j",
 		normalize(os.Getenv("AUTO_JOB_NAME")),
 		"The job name to use for naming the pod")
 
-	if err := runCommand.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	return cmd
 }
 
 func normalize(s string) string {
